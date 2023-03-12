9b719a0931a80006580fb0fcb95e31af86f93de2
bobonline19 <bobonline19@googlemail.com>
2023-03-12 22:30:52 +0000

    well we can me the graph

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+
+Console.WriteLine("Hello, World!");
+var scopes = new[] { "User.Read", "Group.Read.All", "Tasks.Read" };
+var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions
+{
+  ClientId = Secrets.ClientID
+};
+
+string groupName = "Orders";
+string planName = "ShipOrders";
+
+var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
+
+var graphClient = new GraphServiceClient(tokenCredential, scopes);
+
+var me = await graphClient.Me.GetAsync();
+
+var groups = await graphClient.Groups.GetAsync();
+//var group = groups.Where(g => g.Name == groupName).Select(g => g.Id).FirstOrDefault();
+var plans = await graphClient.Planner.Plans.GetAsync();
+//var result = await graphClient.Planner.Plans["{plannerPlan-id}"].Tasks.GetAsync();
+
+
+Console.WriteLine($"Hello {me?.DisplayName}!");
