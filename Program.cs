5e8b6584dbc3b9bff2e3834fcae73e4f7387afae
bobonline19 <bobonline19@googlemail.com>
2023-03-15 00:09:12 +0000

    more trouble with new C# than graph API

--- Program.cs
+++ Program.cs
@@ -1,26 +1,47 @@
 ﻿using Azure.Identity;
 using Microsoft.Graph;
 
-Console.WriteLine("Hello, World!");
-var scopes = new[] { "User.Read", "Group.Read.All", "Tasks.Read" };
-var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions
+namespace PlannerPoc
 {
-  ClientId = Secrets.ClientID
-};
 
-string groupName = "Orders";
-string planName = "ShipOrders";
+  public static class Program
+  {
 
-var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
+    public static async Task Main(StringSplitOptions[] args)
+    {
+      Console.WriteLine("Hello, World!");
+      var scopes = new[] { "User.Read", "Group.Read.All", "Tasks.Read" };
+      var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions
+      {
+        ClientId = Secrets.ClientID
+      };
 
-var graphClient = new GraphServiceClient(tokenCredential, scopes);
+      string groupName = "Orders";
+      string planName = "ShipOrders";
 
-var me = await graphClient.Me.GetAsync();
+      var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
 
-var groups = await graphClient.Groups.GetAsync();
-//var group = groups.Where(g => g.Name == groupName).Select(g => g.Id).FirstOrDefault();
-var plans = await graphClient.Planner.Plans.GetAsync();
-//var result = await graphClient.Planner.Plans["{plannerPlan-id}"].Tasks.GetAsync();
+      var graphClient = new GraphServiceClient(tokenCredential, scopes);
 
+      var me = await graphClient.Me.GetAsync();
 
-Console.WriteLine($"Hello {me?.DisplayName}!");
+      var groups = await graphClient.Groups.GetAsync(r => r.QueryParameters.Filter=$"displayName eq '{groupName}'");
+      var group = groups.Value.AsEnumerable().Single();
+      //var group = groups.Where(g => g.Name == groupName).Select(g => g.Id).FirstOrDefault();
+
+      var plans = await graphClient.Groups[group.Id].Planner.Plans.GetAsync();// .GetAsync (r => r.QueryParameters.Filter=$"title eq '{planName}'");
+      var plan = plans.Value.Where(p => p.Title == planName).SingleOrDefault();
+
+      var tasks = await graphClient.Planner.Plans[plan.Id].Tasks.GetAsync ();
+
+      List<TaskDto> taskData = tasks.Value.Select(t => new TaskDto(t.Id, t.BucketId, t.Title)).ToList();
+      ////var result = await graphClient.Planner.Plans["{plannerPlan-id}"].Tasks.GetAsync();
+
+      var contents = System.Text.Json.JsonSerializer.Serialize(taskData);
+      var groupDrive = await graphClient.Groups[group.Id].Drive.GetAsync(r => r.QueryParameters.Expand=new string[]{"children($select=id,name)"});
+      var root = groupDrive.Root;
+
+      Console.WriteLine($"Hello {me?.DisplayName}!");
+    }
+  }
+}
